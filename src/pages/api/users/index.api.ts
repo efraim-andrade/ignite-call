fa43ab9fd6b6f0799a890b08282c971b0d9f9a42
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-06-02 13:13:34 -0300

    added get-week-days util and hook form on time intervals

--- src/pages/api/users/index.api.ts
+++ src/pages/api/users/index.api.ts
@@ -29,6 +29,7 @@ export default async function handler(
       username,
     },
   })
+  console.log("🚀 ~ user:", user);
 
   setCookie({ res }, '@ignite-call:userId', user.id, {
     maxAge: 60 * 60 * 24 * 7, // 7 days
--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -7,11 +7,47 @@ import {
   TextInput,
 } from '@molao-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { z } from 'zod'
+import { useFieldArray, useForm } from 'react-hook-form'
+
+import { getWeekDays } from '~/utils/get-week-days'
 
 import { Container, Header } from '../styles'
 import * as S from './styles'
 
+const timeIntervalsFormSchema = z.object({})
+
 export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { isSubmiting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const weekDays = getWeekDays()
+
+  const { fields } = useFieldArray({
+    name: 'intervals',
+    control,
+  })
+
+  async function handleSetTimeIntervals() {
+    console.log('submit')
+  }
+
   return (
     <Container>
       <Header>
@@ -22,23 +58,37 @@ export default function TimeIntervals() {
           semana.
         </Text>
 
-        <MultiStep size={4} currentStep={2} />
+        <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <S.IntervalBox as="form">
+      <S.IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
         <S.IntervalsContainer>
-          <S.IntervalItem>
-            <S.IntervalDay>
-              <Checkbox />
-              <Text>Segunda-feira</Text>
-            </S.IntervalDay>
+          {fields.map((field, index) => {
+            return (
+              <S.IntervalItem key={field.id}>
+                <S.IntervalDay>
+                  <Checkbox />
+                  <Text>{weekDays[field.weekDay]}</Text>
+                </S.IntervalDay>
 
-            <S.IntervalInputs>
-              <TextInput size="sm" type="time" step={60} />
+                <S.IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
 
-              <TextInput size="sm" type="time" step={60} />
-            </S.IntervalInputs>
-          </S.IntervalItem>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </S.IntervalInputs>
+              </S.IntervalItem>
+            )
+          })}
         </S.IntervalsContainer>
 
         <Button type="submit">
