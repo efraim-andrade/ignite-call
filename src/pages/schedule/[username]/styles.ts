57d5527cf39c0906ca9b8406b1bc692d747f2700
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-06-12 20:26:37 -0300

    initiating the schedule page

---
+++ src/pages/schedule/[username]/styles.ts
@@ -0,0 +1,23 @@
+import { Heading, Text, styled } from '@molao-ui/react'
+
+export const Container = styled('div', {
+  maxWidth: 852,
+  padding: '$0 $4',
+  margin: '$20 auto $4',
+})
+
+export const UserHeader = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+
+  [`> ${Heading}`]: {
+    marginTop: '$2',
+
+    lineHeight: '$base',
+  },
+
+  [`> ${Text}`]: {
+    color: '$gray200',
+  },
+})
