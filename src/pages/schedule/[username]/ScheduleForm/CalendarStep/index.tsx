d8ada34c98c3cd3281b723ffeb1e3cd3424260dd
Efraim Andrade <efraim_andrade@Efraims-MacBook-Air.local>
2023-06-13 15:07:56 -0300

    building the time picker structure and styles

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -3,9 +3,35 @@ import { Calendar } from '~/components/Calendar'
 import * as S from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <S.Container>
+    <S.Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <S.TimePicker>
+          <S.TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </S.TimePickerHeader>
+
+          <S.TimePickerList>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem disabled>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>08:00h</S.TimePickerItem>
+            <S.TimePickerItem>18:00h</S.TimePickerItem>
+          </S.TimePickerList>
+        </S.TimePicker>
+      )}
     </S.Container>
   )
 }
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -1,13 +1,90 @@
-import { Box, styled } from '@molao-ui/react'
+import { Box, Text, styled } from '@molao-ui/react'
 
 export const Container = styled(Box, {
   position: 'relative',
 
   display: 'grid',
-  gridTemplateColumns: '1fr',
 
   padding: 0,
-  width: 540,
+
   maxWidth: '100%',
   margin: '$6 auto 0',
+
+  variants: {
+    isTimePickerOpen: {
+      true: {
+        gridTemplateColumns: '1fr 280px',
+
+        '@media (max-width: 900px)': {
+          gridTemplateColumns: '1fr',
+        },
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: '1fr',
+      },
+    },
+  },
+})
+
+export const TimePicker = styled('div', {
+  borderLeft: '1px solid $gray600',
+  padding: '$6 $6 0',
+  overflowY: 'scroll',
+
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  right: 0,
+  width: 280,
+})
+
+export const TimePickerHeader = styled(Text, {
+  fontWeight: '$medium',
+
+  span: {
+    color: '$gray200',
+  },
+})
+
+export const TimePickerList = styled('div', {
+  marginTop: '$3',
+
+  gap: '$2',
+  display: 'grid',
+  gridTemplateColumns: '1fr',
+
+  '@media (max-width: 900px)': {
+    gridTemplateColumns: '1fr 1fr',
+  },
+})
+
+export const TimePickerItem = styled('button', {
+  border: 0,
+  padding: '$2 0',
+  borderRadius: '$sm',
+
+  fontSize: '$sm',
+  color: '$gray100',
+  cursor: 'pointer',
+  lineHeight: '$base',
+  backgroundColor: '$gray600',
+
+  '&:last-child': {
+    marginBottom: '$6',
+  },
+
+  '&:disabled': {
+    opacity: 0.4,
+    cursor: 'default',
+    background: 'none',
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:focus': {
+    boxShadow: '0 0 0 2px $colors$gray500',
+  },
 })
